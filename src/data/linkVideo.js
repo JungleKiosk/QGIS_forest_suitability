54246b4364c582eb8b329822dc1d8117d7997958
Fra <135766845+JungleKiosk@users.noreply.github.com>
2024-03-21 19:30:34 +0100

    import data JS

---
+++ src/data/linkVideo.js
@@ -0,0 +1,20 @@
+export const links = [
+    {
+        img: "video/slope_mean_exp.jpg",
+        title: 'Slope mean',
+        desc: "",
+        video: "https://www.youtube.com/watch?v=ELM4dy68G8k&list=PLR8J_sq3CKNdKkFY6pXbylORmVISPp0NL",
+    },
+    {
+        img: "video/dist_mean_exp.jpg",
+        title: 'Distance mean',
+        desc: "",
+        video: "https://www.youtube.com/watch?v=1Z_jDxeNvf0&list=PLR8J_sq3CKNdKkFY6pXbylORmVISPp0NL&index=2",
+    },
+    {
+        img: "video/hippieg.png",
+        title: 'Suitability forestry work',
+        desc: "",
+        video: "https://www.youtube.com/watch?v=yzhsDHsf7J8&list=PLR8J_sq3CKNdKkFY6pXbylORmVISPp0NL&index=3",
+    }
+]
