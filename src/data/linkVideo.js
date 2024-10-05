9bc0585e875c481a573908d56b5163da02c1b61a
Fra <fra24eso@gmail.com>
2024-10-05 02:49:20 +0200

    update links

--- src/data/linkVideo.js
+++ src/data/linkVideo.js
@@ -3,18 +3,18 @@ export const links = [
         img: "video/slope_mean_exp.jpg",
         title: 'Slope mean',
         desc: "",
-        video: "https://www.youtube.com/watch?v=ELM4dy68G8k&list=PLR8J_sq3CKNdKkFY6pXbylORmVISPp0NL",
+        video: "https://youtu.be/PqLFOAOCsBU?si=_J7d_2NE0M5BoWWZ",
     },
     {
         img: "video/dist_mean_exp.jpg",
         title: 'Distance mean',
         desc: "",
-        video: "https://www.youtube.com/watch?v=1Z_jDxeNvf0&list=PLR8J_sq3CKNdKkFY6pXbylORmVISPp0NL&index=2",
+        video: "https://youtu.be/Isn8BrZswUQ?si=EiVUdr4bCDCmj5O6",
     },
     {
         img: "video/hippieg.png",
         title: 'Suitability forestry work',
         desc: "",
-        video: "https://www.youtube.com/watch?v=yzhsDHsf7J8&list=PLR8J_sq3CKNdKkFY6pXbylORmVISPp0NL&index=3",
+        video: "https://youtu.be/R9EWUd0oTYI?si=0X9hZJazb3_2q4xq",
     }
 ]
